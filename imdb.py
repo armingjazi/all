a16a0928157b16478fb041878f7c0671e0239249
tetrohed <armin65@gmail.com>
2019-07-21 17:35:33 +0200

    Add imdb review trainer

---
+++ imdb.py
@@ -0,0 +1,32 @@
+from keras import models, layers
+from keras.datasets import imdb
+import numpy as np
+
+from core.vectorize import vectorize_sequences
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+
+x_train = vectorize_sequences(train_data)
+x_test = vectorize_sequences(test_data)
+y_train = np.asarray(train_labels).astype('float32')
+y_test = np.asarray(test_labels).astype('float32')
+
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+x_val = x_train[:10000]
+partial_x_train = x_train[10000:]
+y_val = y_train[:10000]
+partial_y_train = y_train[10000:]
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=4,
+                    batch_size=512,
+                    validation_data=(x_val, y_val))
